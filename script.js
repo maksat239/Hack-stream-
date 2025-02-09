dc7f03f50c1adc9eaf72f323e7e91712f7d6721a
maksat239 <dosymbai003@gmail.com>
2025-02-09 19:36:17 +0500

    Update script.js
    Signed-off-by: maksat239 <dosymbai003@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+let likeCount = 0;
+const likesPerClick = 100000;  // Бір басқанда қосылатын лайк саны
+
 function startStream() {
     let link = document.getElementById("streamLink").value;
+
     if (link.includes("tiktok.com")) {
         document.getElementById("streamFrame").innerHTML = 
-            `<iframe src="${link}" width="80%" height="500px" allowfullscreen></iframe>`;
+            `<iframe src="${link}" allowfullscreen></iframe>`;
+        document.getElementById("likeSection").style.display = "block";  // Лайк бөлімін көрсету
     } else {
         alert("Дұрыс TikTok стрим сілтемесін енгіз!");
     }
+}
+
+function likeStream() {
+    likeCount += likesPerClick;  // Лайк санын 100000-ға арттыру
+    document.getElementById("likeCount").innerText = `Лайк: ${likeCount}`;
 }
